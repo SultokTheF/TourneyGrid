08e27e9d63e5764551335d8276bc8cd230bf038d
sultok <sultok.003@gmail.com>
2023-07-28 23:45:11 +0600

    implementation react-tournament-bracket package

---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TournamentGrid from "../modules/Bracket/TournamentGrid";
+
+const Main = () => {
+  return (
+    <div>
+      <TournamentGrid />
+    </div>
+  );
+};
+
+export default Main;
