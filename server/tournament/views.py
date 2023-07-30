f1c4adb508ea06a5feb54c4a32e3e0710f2c9e04
sultok <sultok.003@gmail.com>
2023-07-31 01:28:21 +0600

    added sorting by id to the models

--- server/tournament/views.py
+++ server/tournament/views.py
@@ -6,17 +6,28 @@ class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+    def get_queryset(self):
+        return Team.objects.all().order_by('id')
+
 
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+
+    def get_queryset(self):
+        return Match.objects.all().order_by('id')
 
 
 class RoundViewSet(viewsets.ModelViewSet):
     queryset = Round.objects.all()
     serializer_class = RoundSerializer
 
+    def get_queryset(self):
+        return Round.objects.all().order_by('id')
 
 class DoubleEliminationBracketViewSet(viewsets.ModelViewSet):
     queryset = DoubleEliminationBracket.objects.all()
     serializer_class = DoubleEliminationBracketSerializer
+
+    def get_queryset(self):
+        return DoubleEliminationBracket.objects.all().order_by('id')
