4340f9618f048066c450e74bb1024f8596da214d
SultokTheFe <sultok.003@gmail.com>
2023-12-01 00:24:48 +0100

    added user login and JWT authorization

--- server/tournament/urls.py
+++ server/tournament/urls.py
@@ -2,7 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = DefaultRouter()
+router.register(r'users', UserViewSet)
 router.register(r'teams', TeamViewSet)
 router.register(r'matches', MatchViewSet)
 router.register(r'rounds', RoundViewSet)
@@ -10,4 +16,7 @@ router.register(r'double_elimination_brackets', DoubleEliminationBracketViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- server/tournament/views.py
+++ server/tournament/views.py
@@ -1,6 +1,35 @@
+#REST
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+#JWT
+
+
+#
 from .models import *
 from .serializers import *
+
+class UserRegistrationAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.all().order_by('id')
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
@@ -31,3 +60,10 @@ class DoubleEliminationBracketViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return DoubleEliminationBracket.objects.all().order_by('id')
+    
+class MyProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Your view logic here
+        return Response({"message": "You have access to this data."})
